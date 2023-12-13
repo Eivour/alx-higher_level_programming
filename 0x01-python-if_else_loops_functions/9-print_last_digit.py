83b0a4b136c01632fba8f5aa94f527ba85b99a92
Eivour <laragoke91@gmail.com>
2023-12-13 07:43:35 +0100

    Task 9 done

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
 
-
 def print_last_digit(number):
-    # Check if the input is a valid number
-    if not isinstance(number, int):
-        raise ValueError("Input must be an integer")
-
-    # Get the last digit
     last_digit = abs(number) % 10
-
-    # Print the last digit
     print(last_digit, end='')
-
-    # Return the last digit
     return last_digit
 
-
-# Test cases
+# Example usage:
 print_last_digit(98)
 print_last_digit(0)
-result = print_last_digit(-1024)
-print(result)
+r = print_last_digit(-1024)
+print(r)
