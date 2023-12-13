b4a2df505d7344daff0385ee46efcd848f418f0c
Eivour <laragoke91@gmail.com>
2023-12-13 07:40:08 +0100

    Task 8 done

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 
+
 def uppercase(s):
-    result = ''
+    result = ""
     for char in s:
-        result += '{}'.format(chr(ord(char) - 32))
-    
+        if ord('a') <= ord(char) <= ord('z'):
+            uppercase_char = chr(ord(char) - ord('a') + ord('A'))
+            result += "{}".format(uppercase_char)
+        else:
+            result += "{}".format(char)
+
     print(result)
 
-# Example usage:
-uppercase("hello")
+
+if __name__ == "__main__":
+    uppercase("best")
+    uppercase("Best School 98 Battery street")
